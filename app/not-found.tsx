066d9e0cbbfaee00a22164110fda30cda60f1030
Luca Naujoks <luca@bobby68.de>
2024-02-01 14:20:29 +0100

    addited 404 Resource not found custom page

---
+++ app/not-found.tsx
@@ -0,0 +1,15 @@
+"use client"
+import { usePathname } from "next/navigation";
+
+export default function NotFound() {
+    const path = usePathname();
+
+    return (
+        <div className="flex flex-col items-center justify-center h-screen w-screen bg-slate-950 text-gray-200">
+            <div className="flex-col scale-150">
+                <h1 className="text-5xl text-center">404</h1>
+                <p className="text-center">Resource "{path}" not found</p>
+            </div>
+        </div>
+    )
+}
