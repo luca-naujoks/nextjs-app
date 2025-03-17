3bd5b2fb1dc045facad8c1d0106cd60789c003d5
luca-naujoks <luca@bobby68.de>
2025-03-17 21:24:46 +0100

    refactor project page to use Suspense for loading project name

--- app/project/page.tsx
+++ app/project/page.tsx
@@ -1,14 +1,22 @@
 "use client";
 
 import { useRouter, useSearchParams } from "next/navigation";
+import { Suspense } from "react";
 
 export default function Page() {
-  const projectName = useSearchParams().get("project");
   const router = useRouter();
+
+  function ProjectName() {
+    const projectName = useSearchParams().get("project");
+    return <h1>Current Project: {projectName}</h1>;
+  }
+
   return (
     <div>
       <h1>Project pages will be implemented soon</h1>
-      <h1>Current Project: {projectName}</h1>
+      <Suspense fallback={<h1>Loading...</h1>}>
+        <ProjectName />
+      </Suspense>
       <button className="buttonStyle" onClick={() => router.back()}>
         Back
       </button>
