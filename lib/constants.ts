5444af682c3d259e0e74e87464d6e504efa2548b
luca-naujoks <luca@bobby68.de>
2025-11-08 22:32:02 +0100

    add public folder to docker image

--- lib/constants.ts
+++
@@ -1,8 +0,0 @@
-import {ILink} from "@/components/Navigation";
-
-export const links: ILink[] = [
-    {Title: "Home", Link: "/"},
-    {Title: "About Me", Link: "/competencies"},
-    {Title: "Projects", Link: "/projects"},
-    {Title: "Leave a Note", Link: "/leave-a-note"},
-];
