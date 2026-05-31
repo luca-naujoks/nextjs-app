dcdca3d72488be44d3fd7d4c3dc73cf4d1f24596
luca-naujoks <luca@bobby68.de>
2026-05-31 14:32:16 +0200

    feat: further css UX improvements for Mobile devices

--- web-app-solid/src/pages/home.tsx
+++ web-app-solid/src/pages/home.tsx
@@ -6,20 +6,20 @@ import {BackgroundImage} from "../components/BackgroundImage";
 
 export default function Home() {
     return (
-        <div class={"relative w-screen h-screen overflow-hidden"}>
+        <div class={"relative w-screen h-screen"}>
             <div
                 class="absolute top-0 z-5 w-full h-full bg-linear-to-r from-black from-20%  to-black/10 to-100%"/>
             <BackgroundImage src={background}/>
             <div class={"relative z-10 px-4 md:px-16"}>
-                <div class={"flex flex-col justify-center h-full xl:h-screen"}>
+                <div class={"flex flex-col items-center md:items-start justify-center h-full"}>
                     <picture>
                         <source srcset={avatarAVIF} type={"image/avif"}/>
                         <source srcset={avatar} type={"image/webp"}/>
 
-                        <img src={avatar} alt={"casual avatar"} width={320} height={320} class="mt-0 xl:mt-12"/>
+                        <img src={avatar} alt={"casual avatar"} width={320} height={320}/>
                     </picture>
                     <h1 class={"mb-4"}>luca-naujoks</h1>
-                    <p class={"w-full md:w-3/5 lg:w-1/2"}>
+                    <p class={"inline w-2/3 md:w-3/5 lg:w-1/2"}>
                         I’m luca a <span class={"highlight"}>full‑stack</span> developer focused around
                         golang and react.
                         I am building Backends in <span class={"highlight"}>golang</span> with <span
